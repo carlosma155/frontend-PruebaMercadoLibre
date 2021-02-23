b365b35fe688afbf7d718cfd3caf71c4d3744c02
Carlos Benitez <carlosma155@hotmail.com>
2021-02-23 10:28:56 -0500

    Se integró webpack con express, se configuró webpack-dev-middleware y webpack-hot-middleware para que pueda refrescar en tiempo real, tambien se configuró babel

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import webpack from 'webpack';
 import config  from './config';
 
 const app = express();
 const { port, env } = config
 
-if(env === "development") {
-    console.log('Development config')
+if (env === 'development') {
+  console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { publicPath: webpackConfig.output.publicPath };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 }
 
 app.get('*', (req, res) => {
