0b79c75bb0ba653ad85b274c2c8c7a352a952c18
Carlos Benitez <carlosma155@hotmail.com>
2021-02-23 11:11:56 -0500

    Se hicieron configuraciones en webpack y en el servidor para que la app de React este servida desde Express

--- src/server/server.js
+++ src/server/server.js
@@ -18,7 +18,21 @@ if (env === 'development') {
 }
 
 app.get('*', (req, res) => {
-  res.send({ hello: 'express' });
+  res.send(`
+  <!DOCTYPE html>
+    <html lang="es">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="assets/app.css" type="text/css">
+          <title>Prueba Mercado Libre</title>
+      </head>
+      <body>
+          <div id="app"></div>
+          <script src="assets/app.js" type="text/javascript"></script>
+      </body>
+  </html>`);
 });
 
 app.listen(port, (err) => {
